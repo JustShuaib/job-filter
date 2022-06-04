f2c40fb2cdc3d95d87f8a925e23c9588355fa397
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-06-05 00:37:09 +0100

    styled individual job component

--- src/job/Job.jsx
+++ src/job/Job.jsx
@@ -1,7 +1,42 @@
 import React from "react";
-
+import "./job.scss";
+import logo from "../assets/images/photosnap.svg";
 const Job = () => {
-  return <div className="job">job</div>;
+  return (
+    <div className="job">
+      <img src={logo} alt="logo" className="logo" />
+      <section>
+        <div className="title">
+          <p>Photosnap</p>
+          <span className="new">new!</span>
+          <span className="featured">featured</span>
+        </div>
+        <h2>Senior Frontend Developer</h2>
+        <ul className="detail">
+          <li>1d ago</li>
+          <li>Full Time</li>
+          <li>USA only</li>
+        </ul>
+      </section>
+      <ul className="btn-list">
+        <li>
+          <button>Frontend</button>
+        </li>
+        <li>
+          <button>Senior</button>
+        </li>
+        <li>
+          <button>HTML</button>
+        </li>
+        <li>
+          <button>CSS</button>
+        </li>
+        <li>
+          <button>JavaScript</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Job;
